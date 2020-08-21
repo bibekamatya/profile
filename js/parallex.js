3cdddf6e4a7999583170203e86dadfb8f084ee71
Hom Narayan Amatya <58105681+bibekamatya@users.noreply.github.com>
2020-08-21 23:24:23 +0545

    A well formatted files

---
+++ js/parallex.js
@@ -0,0 +1,7 @@
+     $(window).scroll(function(e){
+         parallax();
+         });
+         function parallax(){
+         var scrolled = $(window).scrollTop();
+         $('.background').css('top',-(scrolled*0.15)+'px');
+         }
